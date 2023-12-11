8cd45bfddf53df852e8bc59364580dd1f2150f5f
Saran <113179649+Santosaran@users.noreply.github.com>
2023-12-11 23:51:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
 
-const randomFunc = {
+const randomFunc = { 
 	lower: getRandomLower,
 	upper: getRandomUpper,
 	number: getRandomNumber,
